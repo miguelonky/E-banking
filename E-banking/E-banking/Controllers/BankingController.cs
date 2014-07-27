d2cbeb8b645ad8596a38a37956518a15f5ff3c84
Leonal Hernandez Veloz <lhernandezveloz@gmail.com>
2014-07-27 10:11:39 -0300

    modified view

---
+++ E-banking/E-banking/Controllers/BankingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_banking.Controllers
+{
+    public class BankingController : Controller
+    {
+        //
+        // GET: /Banking/
+
+        public ActionResult HomeBank()
+        {
+            return View();
+        }
+
+    }
+}
