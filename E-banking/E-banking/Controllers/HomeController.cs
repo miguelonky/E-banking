f5a25ca819e603076fef2d134ff5c8a76419c554
miguelonky <20121162@itla.edu.do>
2014-07-26 22:35:40 -0400

    trabajando en el login

--- E-banking/E-banking/Controllers/HomeController.cs
+++ E-banking/E-banking/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using E_banking.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,18 @@ namespace E_banking.Controllers
 {
     public class HomeController : Controller
     {
+        //system.data
+
+        protected DataSet _reportData;
+        protected DataSet _reportFilterTable;
+        protected DataSet _reportArguments;
+        protected DataTable _reportTable;
+
+
+        //objeto de la clase modelo de consultas
+        consultas Cs = new consultas();
+
+
         //
         // GET: /TestView/
 
@@ -24,8 +38,19 @@ namespace E_banking.Controllers
         [HttpPost]
         public ActionResult Login(string usuario, string contraseña)
         {
-          
+            //no eh echo mas porque no eh creado la base de datos aun 
+            string user = usuario;
+            string pass = contraseña;
 
+            /*if (Cs.Autenticar(user, pass))
+            {
+                FormsAuthentication.SetAuthCookie(user, true);
+                return RedirectToAction("index", "Cliente");
+            }
+            {
+              ViewBag.error = "Usuario o contraseña incorrecta";
+            }*/
+           
             return View();
         }
     }
---
+++ E-banking/E-banking/Models/consultas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace E_banking.Models
+{
+    public class consultas
+    {
+        protected DataSet _reportData;
+        protected DataSet _reportFilterTable;
+        protected DataSet _reportArguments;
+        protected DataTable _reportTable;
+
+        public virtual void Autenticar(string usuario, string contraseña)
+        {
+            var da = new SqlDataAccess("UseConfig");
+
+            da.SetProc("Autenticar");
+            da.AddParameter("@usuario", usuario);
+            da.AddParameter("@contraseña", contraseña);
+            da.ExecuteDataSet();
+
+
+
+        }
+    }
+}
