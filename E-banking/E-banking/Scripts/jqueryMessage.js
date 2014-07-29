dd37ef22d5c4446d6e2b7ce2f1ebce31b064b2a8
erick0040 <erick_rodriguez_eldiro@hotmail.es>
2014-07-29 16:57:02 -0400

    The addUsers now validate and other some changes
    xD

---
+++ E-banking/E-banking/Scripts/jqueryMessage.js
@@ -0,0 +1,19 @@
+﻿jQuery.extend(jQuery.validator.messages, {
+    required: "Debe completar este campo.",
+    remote: "Please fix this field.",
+    email: "Introduzca un email valido.",
+    url: "Introduzca una URL valida.",
+    date: "Inserte una fecha valida.",
+    dateISO: "Inserte una fecha valida (ISO).",
+    number: "Inserte un numero valido.",
+    digits: "Introduzca solo numeros.",
+    creditcard: "Introduzca un numero de tarjeta valido.",
+    equalTo: "Introduzca el mismo valor nuevamente.",
+    accept: "Introduzca un valor con extension valida.",
+    maxlength: jQuery.validator.format("No introduzca mas de {0} caracteres."),
+    minlength: jQuery.validator.format("Introduzca al menos {0} caracteres."),
+    rangelength: jQuery.validator.format("Introduzca un valor entre {0} y {1} caracteres."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Introduzca un valor menor o igual a {0}."),
+    min: jQuery.validator.format("Introduzca un valor igual o mayor a {0}.")
+});
--- E-banking/E-banking/Scripts/validateAddUsers.js
+++ E-banking/E-banking/Scripts/validateAddUsers.js
@@ -1,5 +1,40 @@
-function validateUser() {
-	$('fname').validate(
-		
-	);
-}
+$(function() {
+    $('#formAddUser').validate({
+        rules: {
+            fname: {
+                required: true,
+                minlength: 3,
+                maxlength: 45
+            },
+            lname: {
+                required: true,
+                minlength: 3,
+                maxlength: 45
+            },
+            cedula: {
+                required: true,
+                number: true,
+                minlength: 11,
+                maxlength: 11
+            },
+            email: {
+                required: true,
+                email: true,
+                minlength: 5,
+                maxlength: 100
+            },
+            phone: {
+                required: true,
+                number: true,
+                minlength: 10,
+                maxlength: 10
+            },
+            birthdate: {
+                required: true,
+                date: true,
+                minlength: 3,
+                maxlength: 45
+            }
+        }
+    });
+})
